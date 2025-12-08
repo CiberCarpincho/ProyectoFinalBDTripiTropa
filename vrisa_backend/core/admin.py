a3d7aee31aaac24a614500613cabb22eb0b316ce
TofiTurtle <juan.rodriguez.lozano@correounivalle.edu.co>
2025-12-08 03:32:56 -0500

    se añade contenido a admin.py para poder tener intefaz visual linda con django. lso registros ingresados en baq se ven desde la console de fron bien!

--- vrisa_backend/core/admin.py
+++ vrisa_backend/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import (
+    User, Institute, Colors, Station,
+    Device, Alert, Access,
+    UserRegisterInstitute, UserRegisterStation
+)
 
-# Register your models here.
+# Registrar todos los modelos en el admin
+admin.site.register(User)
+admin.site.register(Institute)
+admin.site.register(Colors)
+admin.site.register(Station)
+admin.site.register(Device)
+admin.site.register(Alert)
+admin.site.register(Access)
+admin.site.register(UserRegisterInstitute)
+admin.site.register(UserRegisterStation)
